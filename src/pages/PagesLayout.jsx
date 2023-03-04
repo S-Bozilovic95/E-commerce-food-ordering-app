6686f2100788b9a49c116c5460d8b7bde11d2b55
Lacedaimon95 <svetozar95@gmail.com>
2023-03-04 23:45:37 +0100

    feat: added navigation fixes

--- src/pages/PagesLayout.jsx
+++ src/pages/PagesLayout.jsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 import MainNavigation from "../components/MainNavigation";
 
 const PagesLayout = () => {
@@ -6,6 +7,7 @@ const PagesLayout = () => {
     <div>
       <MainNavigation />
       <Outlet />
+      <Footer />
     </div>
   );
 };
