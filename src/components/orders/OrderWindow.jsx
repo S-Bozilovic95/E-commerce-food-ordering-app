56ae2725d8fb773359277b0c63db896b0ed40b3b
Lacedaimon95 <svetozar95@gmail.com>
2023-03-25 01:58:01 +0100

    feat: style fixes

--- src/components/orders/OrderWindow.jsx
+++ src/components/orders/OrderWindow.jsx
@@ -1,11 +1,21 @@
+import { useSelector } from "react-redux";
+import { cartItemsList } from "../../store/cartSlice";
 import PopUp from "../UI/PopUp";
 import classes from "./OrderWindow.module.scss";
 
 const OrderWindow = ({ showCartHandler }) => {
+  const cartItems = useSelector(cartItemsList);
+
   return (
     <PopUp onClose={showCartHandler.bind(this, false)}>
       <div className={classes.orders}>
-        <p>asdasdasd</p>
+        {cartItems.map((item) => (
+          <div>
+            <img src={item.image} alt="meal" />
+            <p>{item.name}</p>
+            <p>{item.price}</p>
+          </div>
+        ))}
       </div>
     </PopUp>
   );
