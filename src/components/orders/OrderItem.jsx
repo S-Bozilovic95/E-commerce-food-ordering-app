043b354994185d63f824556f9898afd5f069927c
Lacedaimon95 <svetozar95@gmail.com>
2023-03-31 02:28:23 +0200

    feat: added cart slice changes

--- src/components/orders/OrderItem.jsx
+++ src/components/orders/OrderItem.jsx
@@ -1,17 +1,44 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { updateCartItemAmount } from "../../api/cart/cart";
+import { getCartData } from "../../store/cartSlice";
+
 const OrderItem = ({ singleMeal }) => {
-  const changeAmount = () => {
-    // singleMeal.amount = singleMeal.amount + 1;
+  const dispatch = useDispatch();
+  const [updatedItem, setUpdatedItem] = useState({
+    id: singleMeal.id,
+    name: singleMeal.name,
+    description: singleMeal.description,
+    amount: 1,
+    price: singleMeal.price,
+    image: singleMeal.image,
+    category: singleMeal.category,
+  });
+
+  console.log(updatedItem);
+  const amountIncreaseHandler = async () => {
+    setUpdatedItem((prevUpdatedItem) => ({
+      ...prevUpdatedItem,
+      amount: prevUpdatedItem.amount + 1,
+      price: prevUpdatedItem.price + singleMeal.price,
+    }));
+    try {
+      await updateCartItemAmount(singleMeal.id, updatedItem);
+      dispatch(getCartData());
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div>
-      <div onClick={changeAmount}>
+      <div>
         <img src={singleMeal.image} alt="meal" />
         <p>{singleMeal.name}</p>
         <p>{singleMeal.price}</p>
         <span>-</span>
         <p>{singleMeal.amount}</p>
-        <span>+</span>
+        <span onClick={amountIncreaseHandler}>+</span>
       </div>
     </div>
   );
